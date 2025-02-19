51c4aef0fd003d3eeed3459310413776ff533301
Oscar <orss4123@gmail.com>
2025-02-19 15:36:53 -0500

    change to 29 X 29

--- qrCode/scripts/scripts.js
+++ qrCode/scripts/scripts.js
@@ -2,23 +2,48 @@ const canvas = document.getElementById("qrCode");
 const ctx = canvas.getContext("2d");
 const startx = 40;
 const starty = 40;
+const moduleSize = 29;
 const qrCode = [];
+let url = "https://github.com/orss01";
 
-for(let i = 0; i < 25; i++)
+for(let i = 0; i < moduleSize; i++)
 {
     qrCode.push([]);
-    for(let j = 0; j < 25; j++)
+    for(let j = 0; j < moduleSize; j++)
     {
-        qrCode[i].push(0);
+        qrCode[i].push("0");
     }
 }
-
-let url = "https://github.com/orss01";
 
 function drawSquare(x,y)
 {
     ctx.fillRect(startx + (20 * y), starty + (x * 20), 20, 20);
 }
+
+function drawRedSquare(x,y)
+{
+    ctx.fillStyle = "Red";
+    ctx.fillRect(startx + (20 * y), starty + (x * 20), 20, 20);
+}
+
+function drawBlueSquare(x,y)
+{
+    ctx.fillStyle = "Blue";
+    ctx.fillRect(startx + (20 * y), starty + (x * 20), 20, 20);
+}
+
+function drawGreenSquare(x,y)
+{
+    ctx.fillStyle = "Green";
+    ctx.fillRect(startx + (20 * y), starty + (x * 20), 20, 20);
+}
+
+function drawPurpleSquare(x,y)
+{
+    ctx.fillStyle = "Purple";
+    ctx.fillRect(startx + (20 * y), starty + (x * 20), 20, 20);
+}
+
 
 function toBinary(input)
 {
@@ -31,13 +56,13 @@ function toBinary(input)
 }
 
 
-function urlToBinary(url)
+function urlToBytes(url)
 {
-    let byteValues = [];
+    let bytes = "";
     for(let i = 0; i < url.length; i++)
     {
         let byte = toBinary(url.charCodeAt(i));
-        let byteLength = byte.length
+        let byteLength = bytes.length
         if (byteLength < 8)
         {
             for(let j = 0; j < (8 - byteLength); j++)
@@ -45,10 +70,10 @@ function urlToBinary(url)
                 byte = "0" + byte;
             }
         }
-        byteValues.push(byte);
+        bytes += byte;
     }
 
-    return byteValues;
+    return bytes;
 }
 
 /*
@@ -56,12 +81,12 @@ function urlToBinary(url)
 */
 function timingPattern(qrCode)
 {
-    for(let i = 0; i < 25; i++)
+    for(let i = 0; i < moduleSize; i++)
     {
         if (i % 2 == 0)
         {
-            qrCode[i][6] = 1;
-            qrCode[6][i] = 1;
+            qrCode[i][6] = "tp1";
+            qrCode[6][i] = "tp1";
         }
     }
 }
@@ -74,16 +99,16 @@ function finderPatternBox(qrCode, x, y)
 {
     for(let i = 0; i < 7; i++)
     {
-        qrCode[x][i + y] = 1;
-        qrCode[i + x][y] = 1;
-        qrCode[x + 6][i + y] = 1;
-        qrCode[i + x][y + 6] = 1;
+        qrCode[x][i + y] = "fp1";
+        qrCode[i + x][y] = "fp1";
+        qrCode[x + 6][i + y] = "fp1";
+        qrCode[i + x][y + 6] = "fp1";
     }
     for(let i = 0; i < 3; i++)
     {
         for(let j = 0; j < 3; j++)
         {
-            qrCode[x + 2 + i][y + 2 + j] = 1
+            qrCode[x + 2 + i][y + 2 + j] = "fp1";
         }
     }
 }
@@ -93,9 +118,10 @@ function finderPatternBox(qrCode, x, y)
 */
 function finderPattern(qrCode)
 {
+    let offset = moduleSize - 7;
     finderPatternBox(qrCode, 0, 0);
-    finderPatternBox(qrCode, 18, 0);
-    finderPatternBox(qrCode, 0, 18);
+    finderPatternBox(qrCode, offset, 0);
+    finderPatternBox(qrCode, 0, offset);
 }
 
 /*
@@ -103,15 +129,32 @@ function finderPattern(qrCode)
 */
 function alignmentPattern(qrCode)
 {
-    for(let i = 16; i < 21; i++)
+    let offset = moduleSize - 8;
+    for(let i = offset; i < offset + 5; i++)
     {
-        qrCode[i][16] = 1;
-        qrCode[16][i] = 1;
-        qrCode[i][16 + 4] = 1;
-        qrCode[16 + 4][i] = 1;
+        qrCode[i][offset] = "ap1";
+        qrCode[offset][i] = "ap1";
+        qrCode[i][offset + 4] = "ap1";
+        qrCode[offset + 4][i] = "ap1";
     }
 
-    qrCode[18][18] = 1;
+    qrCode[offset + 2][offset + 2] = "ap1";
+}
+
+/*
+    @param qrCode - 2D array of the qrCode layout
+*/
+function formatBits(qrCode)
+{
+    qrCode[moduleSize - 8][8] = "fb1"
+}
+
+/*
+    @param qrCode - 2D array of the qrCode layout
+*/
+function data(qrCode)
+{
+    let data = "0100" + urlToBytes(url) + "0000";
 }
 
 function loadQRCode(qrCode) 
@@ -119,18 +162,34 @@ function loadQRCode(qrCode)
     timingPattern(qrCode);
     finderPattern(qrCode);
     alignmentPattern(qrCode);
-    qrCode[17][8] = 1
+    formatBits(qrCode);
 }
 
 function drawQRCode(qrCode)
 {
-    for (let i = 0; i < 25; i++)
+    for (let i = 0; i < moduleSize; i++)
     {
-        for (let j = 0; j < 25; j++)
+        for (let j = 0; j < moduleSize; j++)
         {
             if(qrCode[i][j] == 1)
             {
                 drawSquare(i,j);
+            }
+            else if(qrCode[i][j] == "fp1")
+            {
+                drawRedSquare(i,j);
+            }
+            else if(qrCode[i][j] == "tp1")
+            {
+                drawBlueSquare(i,j);
+            }
+            else if(qrCode[i][j] == "ap1")
+            {
+                drawGreenSquare(i,j);
+            }
+            else if(qrCode[i][j] == "fb1")
+            {
+                drawPurpleSquare(i,j);
             }
         }
     }
@@ -142,3 +201,7 @@ for(let e of qrCode)
 {
     console.log(e);
 }
+
+let burl = "0100" + urlToBytes(url) + "0000";
+
+console.log(burl);
